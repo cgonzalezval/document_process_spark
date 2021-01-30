8da9e1a14324d7bf11c878fdf295cdea840edf1b
Carlos Gonzalez <carlos.gonzalez.valderrama@gmail.com>
2021-01-30 18:58:30 +0100

    Aux function to sanitize <p> fields in xlm

--- src/python/app/utils.py
+++ src/python/app/utils.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 from pyspark.sql import SparkSession
 
 
@@ -14,3 +15,35 @@ def create_spark_session(name: str, local=False, config=None) -> SparkSession:
             spark = spark.config(key, value)
     spark = spark.getOrCreate()
     return spark
+
+
+def sanitize_xml(data: str) -> str:
+    """Removes all html tags inside <p> tags"""
+    output_data = ""
+    pending = True
+    while pending:
+        p_init = data.find("<p")
+        if p_init != -1:
+            output_data += data[:p_init + 2]
+            data = data[p_init + 2:]
+            if data[0] in [" ", ">"]:  # To match only with <p id=... or <p>, not <pub>
+                position_end_tag = data.find(">") + 1  # 1 position to include >
+                output_data += data[:position_end_tag]
+                data = data[position_end_tag:]
+
+                p_end = data.find("</p>")
+                text_to_sanitize = data[:p_end]
+                text_to_sanitize = re.sub("<[^>]*>", "", text_to_sanitize, flags=re.DOTALL)
+                output_data += text_to_sanitize + "</p>"
+                data = data[p_end + 4:]  # 4 positions for </p>
+            else:  # Cases to ignore i.e <pub
+                pass
+        else:
+            pending = False
+            output_data += data
+
+    return output_data
+
+# data[p_init + 2]
+# data[:p_init + 2]
+# data[p_init + 2:]
---
+++ src/python/tests/test_utils.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import pytest
+from ..app.utils import sanitize_xml
+
+
+@pytest.mark.parametrize("input_data,expected", [
+    ("text sample", "text sample"),
+    ("<p>text sample</p>", "<p>text sample</p>"),
+    ("<description>text sample</description>", "<description>text sample</description>"),
+    ("<description>text </br> sample</description>", "<description>text </br> sample</description>"),
+    ("<p>text </br> sample</p>", "<p>text  sample</p>"),
+    ("<p col='a'>text </br> sample</p>", "<p col='a'>text  sample</p>"),
+    ("<p>text </br> sample</p><p>other</br></p>", "<p>text  sample</p><p>other</p>"),
+    ("<pub>text </br> sample</pub>", "<pub>text </br> sample</pub>"),
+    ("<p>text</p><desc>text2</br></desc>", "<p>text</p><desc>text2</br></desc>"),
+    ("<pub>text</b></pub><p id=1>text2</br></p>", "<pub>text</b></pub><p id=1>text2</p>"),
+])
+def test_sanitize_xml(input_data, expected):
+    result = sanitize_xml(input_data)
+    assert result == expected
