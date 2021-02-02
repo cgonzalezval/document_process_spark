856e6f0c9871e9999a9a41e16fb31d5f3d574649
Carlos Gonzalez <carlos.gonzalez.valderrama@gmail.com>
2021-02-02 19:28:58 +0100

    Testing frequent words

---
+++ src/python/tests/test_frequent_words.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import os
+import pandas as pd
+
+from ..app.frequent_words import process
+from ..app.filter_english_patents import OUTPUT_COL_ENGLISH_TEXT
+
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+INPUT_FOLDER = "data_frequent_words/"
+INPUT_FOLDER = os.path.join(os.path.dirname(os.path.realpath(__file__)), INPUT_FOLDER)
+
+
+def test_process(spark):
+    text1 = "the patent is a document"
+    text2 = "this is a patent classifier"
+    text3 = "text has to be cleaned in this step"
+    data = {
+        "_file": [1, 2, 3],
+        OUTPUT_COL_ENGLISH_TEXT: [text1, text2, text3]
+    }
+    df = spark.createDataFrame(pd.DataFrame(data))
+    result_p = process(df)
+    expected = {
+        "patent": 2,
+        "document": 1,
+        "classifier": 1,
+        "text": 1,
+        "cleaned": 1,
+        "step": 1
+    }
+    words = []
+    nums = []
+    for word, num in expected.items():
+        words.append(word)
+        nums.append(num)
+    expected_output_p = pd.DataFrame({"word": words, "count": nums})
+    check = pd.merge(result_p, expected_output_p, on=result_p.columns.tolist(), how="outer", indicator=True)
+    errors = check[check["_merge"] != "both"]
+    assert errors.empty, f"There are the following errors:\n{errors.to_string()}"
