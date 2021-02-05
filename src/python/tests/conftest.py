06d52e35db9e64ec484fb5fdfbcb3cf8ca4f1004
Carlos Gonzalez <carlos.gonzalez.valderrama@gmail.com>
2021-02-05 19:04:06 +0100

    Added jar dependencies in resources. In a production environment it should be downloaded in a previous step with maven

--- src/python/tests/conftest.py
+++ src/python/tests/conftest.py
@@ -3,13 +3,13 @@ import findspark
 findspark.init()
 import os
 import pytest
-from utils import create_spark_session, zip_app, ZIP_FILE_NAME, get_app_path
+from utils import create_spark_session, zip_app, ZIP_FILE_NAME, get_app_path, get_resources_path
 
 
 @pytest.fixture(scope='session')
 def spark():
     config = {
-        "spark.jars.packages": "com.databricks:spark-xml_2.11:0.11.0",
+        "spark.jars": os.path.join(get_resources_path(), "spark-xml_2.11-0.11.0.jar"),
     }
     spark = create_spark_session("tests", local=True, config=config)
     zip_app()
