287b91c712486e61afc57cac5d9d87ee812a10df
Carlos Gonzalez <carlos.gonzalez.valderrama@gmail.com>
2021-02-06 22:27:08 +0100

    Bugfix init_cluster. It needs azure package to properly import python scripts

--- src/python/azure_scripts/init_ingest_cluster_nlp.sh
+++ src/python/azure_scripts/init_ingest_cluster_nlp.sh
@@ -2,6 +2,7 @@ echo $DB_CLUSTER_ID
 echo $DB_PYTHON_VERSION
 
 echo 'installing python packages...'
+/databricks/python/bin/pip install azure-storage-blob==12.7.1
 /databricks/python/bin/pip install spark-nlp==2.7.2
 echo 'Installation completed!'
 
